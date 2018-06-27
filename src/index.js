9d37bb32e1068c504f946d7636bd8aa6fc969a0e
DeartF <i.artemon87@gmail.com>
2018-06-27 15:14:33 +0600

    myWebpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import avg from './some.js';
+
+$('.title').html('Some text!')
+
+console.log(avg(1, 4, 8));
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
